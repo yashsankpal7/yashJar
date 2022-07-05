d2cb4409edefed276761a4e5aac35c57cd97aebd
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2022-07-05 22:43:51 +0530

    small calculator

---
+++ src/main/java/com/example/yashJar_1/Mathx.java
@@ -0,0 +1,23 @@
+package com.example.yashJar_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Mathx
+{
+
+    public static int add(int left,int right){
+        return left+right;
+    };
+    public static int subtract(int left,int right){
+        return left-right;
+    };
+    public static int multiply(int left,int right){
+        return left*right;
+    };
+    public static int divide(int left,int right){
+        return left/right;
+    };
+
+}
--- src/test/java/com/example/yashJar_1/AppTest.java
+++ src/test/java/com/example/yashJar_1/AppTest.java
@@ -1,5 +1,6 @@
 package com.example.yashJar_1;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,22 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void addTest(){
+        assertEquals(30,Mathx.add(10,20));
+    }
+
+    @Test
+    public void subtractTest(){
+        assertEquals(-100,Mathx.subtract(10,110));
+    }
+
+    @Test
+    public void multiplyTest(){
+        assertEquals(200,Mathx.multiply(10,20));
+    }
+
+    @Test
+    public void divideTest(){
+        assertEquals(3,Mathx.divide(9,3));
     }
 }
